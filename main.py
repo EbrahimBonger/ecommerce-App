eb5d280520443b9d3927c45015b1afef9a5a8848
Ubuntu <ubuntu@ip-172-31-54-18.ec2.internal>
2021-03-03 23:13:28 +0000

    Login page implemented

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import mysql.connector
 #import sqlite3
 
 app = Flask('app')
+app.secret_key = b'mysecretkey'
 
 mydb = mysql.connector.connect(
     host="ec2-100-26-20-223.compute-1.amazonaws.com",
@@ -21,24 +22,53 @@ def home():
   #return 'Hello World'
   return render_template("index.html")
 
-# @app.route('/login',methods=['GET', 'POST'])
-@app.route('/',methods=['GET', 'POST'])
+@app.route('/login',methods=['GET', 'POST'])
+#@app.route('/',methods=['GET', 'POST'])
 def login():
   if request.method == 'POST':
     username = request.form.get("username")
     password = request.form.get("password")
+    
 
     c = mydb.cursor()
 
-    c.execute('SELECT * FROM user WHERE username=%s AND password=%s', (username, password))
+    c.execute('SELECT * FROM user WHERE username=%s AND passwordHash=%s', (username, password))
 
     result =  c.fetchone()
     if result is None:
       c.close()
-      return render_template('loggedin.html',message='Incorrect username or password')
+      return render_template('register.html',message='Incorrect username or password')
     
     c.close()
     session['username'] = username
-  return render_template('loggedin.html',message="Welcome %s" % (username)) 
+    return render_template('loggedin.html',message="Welcome %s" % (username)) 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+  if request.method == 'POST':
+    fname = request.form.get("fname")
+    lname = request.form.get("lname")
+    email = request.form.get("email")
+    username = request.form.get("user")
+    password = request.form.get("pass")
+
+
+    c = mydb.cursor()
+
+    c.execute('INSERT INTO user (username, first_name, last_name, email, passwordHash)  VALUES (%s, %s, %s, %s, %s)', (username, fname, lname, email, password))
+
+    mydb.commit()
+    c.close()
+    
+    return render_template('registered.html')
+
+  ## Otherwise return register page on get request
+  return render_template('register.html')
+
+@app.route('/user',methods=['GET'])
+def user():
+  return render_template('loggedin.html',message="Hello %s" % (session['username']))
+
 
 app.run(host='0.0.0.0', port=8080)
--- store_schema.sql
+++ store_schema.sql
@@ -11,38 +11,46 @@
 -- add create table for all your tabled
 -- User, Product, Orders, Catagory 
 -- AUTO_INCREMENT = generate a unique identity for new rows
-create table user (
+
+DROP TABLE IF EXISTS user;
+CREATE TABLE `user` (
   `userId` BIGINT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(50) NULL DEFAULT NULL,
-  `middleName` VARCHAR(50) NULL DEFAULT NULL,
-  `lastName` VARCHAR(50) NULL DEFAULT NULL,
+  `first_name` VARCHAR(50) NULL DEFAULT NULL,
+  `last_name` VARCHAR(50) NULL DEFAULT NULL,
   `email` VARCHAR(50) NULL,
   `passwordHash` VARCHAR(32) NOT NULL,
   PRIMARY KEY (`userId`)
 );
 
-INSERT INTO user (username, middleName, lastName)
-VALUES ('value1', 'value2', 'value3'); 
+INSERT INTO `user` ( `username`, `first_name`, `last_name`, `email`, `passwordHash`) VALUES
+('user', 'user', 'user', 'user', 'pass' );
 
-create table product (
+-- INSERT INTO user (username, middleName, lastName)
+-- VALUES ('value1', 'value2', 'value3'); 
+
+DROP TABLE IF EXISTS product;
+CREATE TABLE `product` (
     `productId` BIGINT NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(75) NOT NULL,
     `categoryId` BIGINT NOT NULL,
     `price` FLOAT NOT NULL DEFAULT 0,
     `quantity` SMALLINT(6) NOT NULL DEFAULT 0,
-    PRIMARY KEY (`productId`),
+    PRIMARY KEY (`productId`)
 );
 
-create table category (
+DROP TABLE IF EXISTS category;
+CREATE TABLE `category` (
     `categoryId` BIGINT NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(75) NOT NULL,
     PRIMARY KEY (`categoryId`)
 );
 
-create table order (
+DROP TABLE IF EXISTS orders;
+CREATE TABLE `orders` (
     `orderId` BIGINT NOT NULL AUTO_INCREMENT,
     `userId` BIGINT NULL DEFAULT NULL,
-    `productId` BIGINT NULL DEFAULT NULL,
+    `productId` BIGINT NOT NULL,
     `quantity` SMALLINT(6) NOT NULL DEFAULT 0,
     `date` DATE,
     PRIMARY KEY (`orderId`, `productId`)
