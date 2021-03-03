cfc153cc9737e463427209e9cd343a27abe3ba33
Ubuntu <ubuntu@ip-172-31-54-18.ec2.internal>
2021-03-03 16:09:30 +0000

    Login page implementation in progress

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
 from flask import Flask, session
 from flask import render_template, request
-# import mysql.connector
-import sqlite3
+import mysql.connector
+#import sqlite3
 
 app = Flask('app')
 
+mydb = mysql.connector.connect(
+    host="ec2-100-26-20-223.compute-1.amazonaws.com",
+    user="ebrahim5",
+    password="seas",
+    database="cs2541db"
+)
+
+
+
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
+
+def home():
+  # Render the homepage 
+  #return 'Hello World'
+  return render_template("index.html")
+
+# @app.route('/login',methods=['GET', 'POST'])
+@app.route('/',methods=['GET', 'POST'])
+def login():
+  if request.method == 'POST':
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    c = mydb.cursor()
+
+    c.execute('SELECT * FROM user WHERE username=%s AND password=%s', (username, password))
+
+    result =  c.fetchone()
+    if result is None:
+      c.close()
+      return render_template('loggedin.html',message='Incorrect username or password')
+    
+    c.close()
+    session['username'] = username
+  return render_template('loggedin.html',message="Welcome %s" % (username)) 
 
 app.run(host='0.0.0.0', port=8080)
--- store_schema.sql
+++ store_schema.sql
@@ -4,4 +4,50 @@
 
 -- add create table for all your tabled
 
+-- add drop table for all your tables if they exist
+-- DROP TABLE IF EXISTS table_name CASCADE;
+
+
+-- add create table for all your tabled
+-- User, Product, Orders, Catagory 
+-- AUTO_INCREMENT = generate a unique identity for new rows
+create table user (
+  `userId` BIGINT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(50) NULL DEFAULT NULL,
+  `middleName` VARCHAR(50) NULL DEFAULT NULL,
+  `lastName` VARCHAR(50) NULL DEFAULT NULL,
+  `email` VARCHAR(50) NULL,
+  `passwordHash` VARCHAR(32) NOT NULL,
+  PRIMARY KEY (`userId`)
+);
+
+INSERT INTO user (username, middleName, lastName)
+VALUES ('value1', 'value2', 'value3'); 
+
+create table product (
+    `productId` BIGINT NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(75) NOT NULL,
+    `categoryId` BIGINT NOT NULL,
+    `price` FLOAT NOT NULL DEFAULT 0,
+    `quantity` SMALLINT(6) NOT NULL DEFAULT 0,
+    PRIMARY KEY (`productId`),
+);
+
+create table category (
+    `categoryId` BIGINT NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(75) NOT NULL,
+    PRIMARY KEY (`categoryId`)
+);
+
+create table order (
+    `orderId` BIGINT NOT NULL AUTO_INCREMENT,
+    `userId` BIGINT NULL DEFAULT NULL,
+    `productId` BIGINT NULL DEFAULT NULL,
+    `quantity` SMALLINT(6) NOT NULL DEFAULT 0,
+    `date` DATE,
+    PRIMARY KEY (`orderId`, `productId`)
+);
+
+-- add insert statements to populate your tables
+
 -- add insert statements to populate your tables
