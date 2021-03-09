d6a52cdb15ab069167680f15dc4b3f42bca6a4f9
Ubuntu <ubuntu@ip-172-31-54-18.ec2.internal>
2021-03-09 20:05:46 +0000

    login, register, product & checkoutpage implemented

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, session
-from flask import render_template, request
+# from flask import render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+
 import mysql.connector
 #import sqlite3
 
@@ -13,35 +15,229 @@ mydb = mysql.connector.connect(
     database="cs2541db"
 )
 
-
-
 @app.route('/')
-
 def home():
   # Render the homepage 
   #return 'Hello World'
-  return render_template("index.html")
+  return render_template("login.html")
 
-@app.route('/login',methods=['GET', 'POST'])
+
+
 #@app.route('/',methods=['GET', 'POST'])
+@app.route('/login',methods=['GET', 'POST'])
 def login():
   if request.method == 'POST':
     username = request.form.get("username")
     password = request.form.get("password")
     
 
-    c = mydb.cursor()
+    c = mydb.cursor(buffered=True)
+    # p = mydb.cursor(buffered=True)
+
+    # p.execute("SELECT * FROM product")
+    # products = p.fetchall()
 
     c.execute('SELECT * FROM user WHERE username=%s AND passwordHash=%s', (username, password))
 
-    result =  c.fetchone()
-    if result is None:
+    users = c.fetchone()
+    if users is None:
       c.close()
-      return render_template('register.html',message='Incorrect username or password')
-    
+      return render_template('register.html', message='Please Sign Up first to Login!')
+    mydb.commit()
     c.close()
+
+    # the usesr grabs a cart upon first entry
+    session.modified = True
+    for key in list(session.keys()):
+     session.pop(key)
+    # print(session)
+    if not 'cart_item' in session:
+      
+      session['total_price'] = 0
+      session['cart_item'] = cart_item = {}
+    
     session['username'] = username
-    return render_template('loggedin.html',message="Welcome %s" % (username)) 
+
+    return redirect(url_for('products'))
+
+  
+
+@app.route('/products', methods=['GET', 'POST'])
+def products():
+#  try:
+  # conn = mysql.connect()
+  p = mydb.cursor(buffered=True)
+  p.execute('SELECT * FROM product')
+  
+  rows = p.fetchall()
+  # products = p.fetchall()
+  mydb.commit()
+  p.close() 
+  # print(rows)
+  dict = {}
+  # row[2] represents the category index in the tuple
+  for row in rows:
+    # row['running_qty'] = 0
+    # print("running_qty")
+    # print(row)
+    if row[2] in dict:
+        dict.get(row[2]).append(row)
+    else:
+        dict[row[2]] = [row]
+    
+  # print(dict)
+  return render_template('products.html', dict=dict)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_product_to_cart():
+  session.modified = True
+  c = None  
+  _quantity = int(request.form['quantity'])
+  _title = request.form['title'] # it gives the name user clicked
+  title = request.form['title']
+  print("title "+_title)
+  if _quantity <= 0 or None:
+    return
+  if _title and _quantity and request.method == 'POST':
+
+
+
+
+
+    c = mydb.cursor(buffered=True, dictionary=True)
+    
+    
+    given = _title
+    checkQuery = ("SELECT * FROM product WHERE title=%s")
+    userAuthInfo = (given,)
+    c.execute(checkQuery, userAuthInfo)
+
+    row = c.fetchone()
+
+        # check if the product already added to the cart and the user has modifiyed the quantity
+    if _title in session['cart_item']:
+      
+      modified_item = title
+      old_total_price = session['total_price']
+      old_qty = session['cart_item'][title]['running_qty']
+      item_price = session['cart_item'][title]['price']
+      substract_from_toatal = old_qty * item_price
+      new_total_price = old_total_price - substract_from_toatal
+      session['total_price'] = new_total_price
+
+    session['cart_item'][row['title']] = row
+    print(session)
+    for key, val in session['cart_item'].items():
+      if key == _title:
+        
+        session['cart_item'][key]['running_qty'] = _quantity
+
+        new_quantity = session['cart_item'][key]['quantity'] - _quantity
+        session['cart_item'][key]['quantity'] = new_quantity
+        # print("running_qty ")
+        # print(_quantity)
+        # print(session['cart_item'][key]['running_qty'])
+        # print(session)
+        # session['cart_item'][key]['quantity'] -= _quantity
+        current_products_price = session['cart_item'][key]['price'] * _quantity
+        old_price = session['total_price'] 
+        current_products_price = old_price + current_products_price
+        session['total_price'] = current_products_price 
+      
+      
+
+    
+    mydb.commit()
+
+
+
+  return redirect(url_for('products'))
+
+@app.route('/calculate', methods=['GET', 'POST'])
+def calculate_on_checkout():
+  # c  = mydb.cursor()
+  _quantity = int(request.form['quantity'])
+  _title = request.form['title']
+  print("_title and quantity")
+  print(_title)
+  print(_quantity)
+  
+  if _quantity and _title and request.method == 'POST':
+    
+    
+    all_total_price = 0
+    all_total_quantity = 0
+
+    session.modified = True
+    if 'cart_item' in session:
+    #  if the product already exist, add the quantity and price of the existing
+      if row['title'] in session['cart_item']:
+        for key, value in session['cart_item'].items():
+          old_quantity = session['cart_item'][key]['quantity']
+          total_quantity = old_quantity + _quantity
+          session['cart_item'][key]['quantity'] = total_quantity
+          session['cart_item'][key]['total_price'] = total_quantity * row['price']
+    
+    for key, value in session['cart_item'].items():
+
+     individual_quantity = session['cart_item'][key]['quantity']
+     individual_price = session['cart_item'][key]['price']
+
+     all_total_quantity = all_total_quantity + individual_quantity
+     all_total_price = all_total_price + individual_price
+  else:
+    # session['cart_item'] = itemArray
+    all_total_quantity = all_total_quantity + _quantity
+    all_total_price = all_total_price + _quantity * row['price']
+    
+  session['all_total_quantity'] = all_total_quantity
+  session['all_total_price'] = all_total_price
+
+  return redirect(url_for('checkout'))
+
+@app.route('/delete/<string:title>')
+def delete_product(title):
+ try:
+  all_total_price = 0
+  all_total_quantity = 0
+  session.modified = True
+   
+  for key, val in session['cart_item'].items():
+
+     if title == key:    
+      # adjust the total price
+      removed_price = session['cart_item'][key]['running_qty'] * session['cart_item'][key]['price']
+      old_price = session['total_price']
+      session['total_price'] = old_price - removed_price
+      # session['cart_item'][key]['running_qty']
+      session['cart_item'].pop(key)
+      break
+
+
+   
+  return redirect(url_for('.checkout'))
+ except Exception as e:
+  print(e)
+
+@app.route('/checkout', methods=['GET', 'POST'])
+def checkout():
+
+  return render_template('checkout.html')
+@app.route('/empty', methods=['GET', 'POST'])
+def empty_cart():
+ try:
+  session.modified = True
+  print(session['cart_item'])
+  session['cart_item'] = {}
+  session['total_price'] = 0
+  print(session['cart_item'])
+  return redirect(url_for('.checkout'))
+ except Exception as e:
+  print(e)
+
+
+
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -57,6 +253,7 @@ def register():
     c = mydb.cursor()
 
     c.execute('INSERT INTO user (username, first_name, last_name, email, passwordHash)  VALUES (%s, %s, %s, %s, %s)', (username, fname, lname, email, password))
+    
 
     mydb.commit()
     c.close()
@@ -69,6 +266,29 @@ def register():
 @app.route('/user',methods=['GET'])
 def user():
   return render_template('loggedin.html',message="Hello %s" % (session['username']))
+
+def array_merge( first_array , second_array ):
+ if isinstance( first_array , list ) and isinstance( second_array , list ):
+  return first_array + second_array
+ elif isinstance( first_array , dict ) and isinstance( second_array , dict ):
+  return dict( list( first_array.items() ) + list( second_array.items() ) )
+ elif isinstance( first_array , set ) and isinstance( second_array , set ):
+  return first_array.union( second_array )
+ return False  
+
+def close(self):
+    self.cursor.close()
+    self.connection.close()
+
+def execute(self, sql):
+    return self.cursor.execute(sql)
+
+def fetchall(self):
+    return self.cursor.fetchall()
+
+def commit(self):
+    #self.cursor.close()
+    self.connection.commit() 
 
 
 app.run(host='0.0.0.0', port=8080)
--- store_schema.sql
+++ store_schema.sql
@@ -26,31 +26,61 @@ CREATE TABLE `user` (
 INSERT INTO `user` ( `username`, `first_name`, `last_name`, `email`, `passwordHash`) VALUES
 ('user', 'user', 'user', 'user', 'pass' );
 
--- INSERT INTO user (username, middleName, lastName)
--- VALUES ('value1', 'value2', 'value3'); 
+INSERT INTO `user` ( `username`, `first_name`, `last_name`, `email`, `passwordHash`) VALUES
+('user', 'user', 'user', 'user', 'pass' );
+
+
+
 
 DROP TABLE IF EXISTS product;
 CREATE TABLE `product` (
     `productId` BIGINT NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(75) NOT NULL,
-    `categoryId` BIGINT NOT NULL,
+    `title` VARCHAR(50) NULL DEFAULT NULL,
+    `categoryname` VARCHAR(50) NULL DEFAULT NULL,
     `price` FLOAT NOT NULL DEFAULT 0,
     `quantity` SMALLINT(6) NOT NULL DEFAULT 0,
     PRIMARY KEY (`productId`)
 );
 
+INSERT INTO `product` (`title`, `categoryname`, `price`, `quantity`) VALUES
+('shampoo','self-care', 10, 10);
+
+INSERT INTO `product` (`title`, `categoryname`, `price`, `quantity`) VALUES
+('conditioner','self-care', 10, 10);
+
+INSERT INTO `product` (`title`, `categoryname`, `price`, `quantity`) VALUES
+('lotion','cosmotics', 10, 10);
+
+INSERT INTO `product` (`title`, `categoryname`, `price`, `quantity`) VALUES
+('soap','home-cleaning', 10, 10);
+
+INSERT INTO `product` (`title`, `categoryname`, `price`, `quantity`) VALUES
+('detergent','home-cleaning', 10, 10);
+
+
+
 DROP TABLE IF EXISTS category;
 CREATE TABLE `category` (
     `categoryId` BIGINT NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(75) NOT NULL,
+    `categoryname` VARCHAR(75) NOT NULL,
     PRIMARY KEY (`categoryId`)
 );
+
+INSERT INTO `category` (`categoryname`) VALUES
+('self-care');
+
+INSERT INTO `category` (`categoryname`) VALUES
+('cosmotics');
+
+INSERT INTO `category` (`categoryname`) VALUES
+('home-cleaning');
 
 DROP TABLE IF EXISTS orders;
 CREATE TABLE `orders` (
     `orderId` BIGINT NOT NULL AUTO_INCREMENT,
     `userId` BIGINT NULL DEFAULT NULL,
     `productId` BIGINT NOT NULL,
+    `categoryId` BIGINT NOT NULL,
     `quantity` SMALLINT(6) NOT NULL DEFAULT 0,
     `date` DATE,
     PRIMARY KEY (`orderId`, `productId`)
@@ -58,4 +88,4 @@ CREATE TABLE `orders` (
 
 -- add insert statements to populate your tables
 
--- add insert statements to populate your tables
+
